c7aa63b34a4adbe87c2903450c5cae7bcaa0fec5
EvertonAlmeida <everton.o.almeida@outlook.com>
2025-01-12 12:53:18 +0100

    implement sum functionality

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,5 +8,17 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+    let a: i32 = 5;
+    let b: i32 = 7;
+    let sum: i32 = add_two_numbers(a, b);
+    display_sum(a, b, sum);
+}
 
+fn add_two_numbers(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn display_sum(a: i32, b: i32, sum: i32) {
+    println!("The sum of {} and {} is {:?}", a, b, sum);
+}
