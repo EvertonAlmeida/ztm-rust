854b7800c4d8d26d9b803f05e300aef6b2715cff
EvertonAlmeida <everton.o.almeida@outlook.com>
2025-01-12 17:52:56 +0100

    implement match functionality

--- activities/src/bin/a4a.rs
+++ activities/src/bin/a4a.rs
@@ -7,5 +7,14 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+    let value: bool = true;
+    println!("{}", is_true(value));
+}
 
+fn is_true(value: bool) -> String {
+    match value {
+        true => "it's true".to_string(),
+        false => "it's false".to_string(),
+    }
+}
