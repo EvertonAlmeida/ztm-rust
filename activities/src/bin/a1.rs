faff4e307317728e0ae01a74bef887c29fc90c83
EvertonAlmeida <everton.o.almeida@outlook.com>
2025-01-12 12:15:11 +0100

    implement name display functionality

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,5 +8,16 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let first_name: &str = display_first_name("John");
+    let last_name: &str = display_last_name("Doe");
+    println!("My name is {} {}", first_name, last_name);
+}
 
+fn display_first_name(name: &str) -> &str {
+    name
+}
+
+fn display_last_name(last_name: &str) -> &str {
+    &last_name
+}
