efd47375a1a2c171d6a84de2e83662a2bbd55753
EvertonAlmeida <everton.o.almeida@outlook.com>
2025-01-13 10:25:31 +0100

    implement loop statement

--- activities/src/bin/a5.rs
+++ activities/src/bin/a5.rs
@@ -9,5 +9,17 @@
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+fn main() {
+    display_numbers();
+}
 
+fn display_numbers() {
+    let mut num: i32 = 1;
+    loop {
+        println!("{}", num);
+        num += 1;
+        if num > 4 {
+            break;
+        }
+    }
+}
