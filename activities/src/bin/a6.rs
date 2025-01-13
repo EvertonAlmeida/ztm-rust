e08083dbae69b6074babff6be165c1ab8636a355
EvertonAlmeida <everton.o.almeida@outlook.com>
2025-01-13 10:32:04 +0100

    implement while statement

--- activities/src/bin/a6.rs
+++ activities/src/bin/a6.rs
@@ -10,5 +10,15 @@
 // * Print the variable within the while loop
 // * Do not use break to exit the loop
 
-fn main() {}
+fn main() {
+    display_countdown();
+}
 
+fn display_countdown() {
+    let mut num: i32 = 5;
+    while num > 0 {
+        println!("{}", num);
+        num -= 1;
+    }
+    println!("done!");
+}
